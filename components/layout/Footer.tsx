1036a63549c572ccf0b90d7357eb3bcc173ec01b
Andrew S <andrews54757@gmail.com>
2026-02-05 15:14:34 -0500

    update

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -3,15 +3,10 @@
 import { siteConfig } from "@/lib/siteConfig";
 
 export function Footer() {
-  const repositoryUrl = siteConfig.repositoryUrl ?? "#";
-
   return (
     <footer className="border-t py-6 text-center text-xs text-gray-500 dark:text-gray-400">
-      Built for the {siteConfig.siteName}. See the code on{" "}
-      <a href={repositoryUrl} className="underline">
-        Github
-      </a>
-      .
+      Built for the {siteConfig.siteName}.
+      <a href="https://llamamc.org/" className="underline">Join the Llama Collective</a> to contribute or support development.
     </footer>
   );
 }
